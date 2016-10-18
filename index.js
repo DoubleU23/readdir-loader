f1b5649bc85bb55c5792ee7330a29960c8c83618
DoubleU23 <sf@tailored-digital.com>
2016-10-18 12:31:24 +0200

    running version

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
 /*
-  MIT License http://www.opensource.org/licenses/mit-license.php
-  Author - Stefan Friedl @DoubleU23
+    MIT License http://www.opensource.org/licenses/mit-license.php
+    Author - Stefan Friedl @DoubleU23
 */
 var loaderUtils = require('loader-utils')
 ,   path        = require('path')
 ,   dive        = require('dive')
 
 module.exports = function (content) {
+    // cache in production
+    if (process.env.NODE_ENV === 'production') this.cacheable && this.cacheable()
+    // ensure emitFile
+    if (!this.emitFile) throw new Error('emitFile is required from module system')
+    // loader preset
+    var callback = this.async()
+    ,   query     = loaderUtils.parseQuery(this.query)
 
-  var PATH = this.resourcePath
-  ,   DIR  = path.dirname(PATH)
+    // loader code
+    var PATH        = this.resourcePath
+    ,   DIR         = path.dirname(PATH)
+    ,   matches     = []
+    ,   diveOptions = {
+            directories:  query.directories != null ? query.directories : true
+        ,   basename:     query.basename    != null ? query.basename    : true
+        ,   files:        query.files       != null ? query.files       : false
+        ,   recursive:    query.recursive   != null ? query.recursive   : false
+      }
+    ,   basename    = query.directories != null ? query.directories : true
 
-// console.log('[dirfiles-loader] DIR', DIR)
-// var dirList = []
-// dive(DIR, function(err, file, stat) {
-//   if (stat.isDirectory()) {
-//     console.log('file found', file)
-//     dirList.push(file)
-//   }
-// }, () => {
-//     console.log('done', dirList)
-// })
-
-  if (process.env.NODE_ENV === 'production')
-    this.cacheable && this.cacheable()
-
-  if(!this.emitFile)
-    throw new Error('emitFile is required from module system')
-
-// content.toString(), query, this.options, this
-console.log('\n[dirfiles-loader] content', this.resourcePath)
-console.log('\n[dirfiles-loader]')
-
-  var query     = loaderUtils.parseQuery(this.query)
-  ,   url       = loaderUtils.interpolateName(this, query.name || '[hash].[ext]', {
-      context:    query.context || this.options.context,
-      content:    content,
-      regExp:     query.regExp
+    dive(DIR, diveOptions, (err, file, stat) => {
+        if (err) throw err
+        var match = basename ? path.basename(file) : file
+        matches.push(match)
+    }, () => {
+        var result = 'module.exports =  ' + JSON.stringify(matches)
+        callback(null, result)
     })
-
-  var returnVal = {foo: 'bar'}
-  return 'module.exports =  ' + JSON.stringify(returnVal)
 }
 module.exports.raw = true
